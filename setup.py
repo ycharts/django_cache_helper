d37015a71cb25cdf7080238a73a7785c9743d48d
Hyuckin Lim <hyuckin@Hyuckins-MacBook-Pro.local>
2020-03-20 11:36:21 -0400

    increase version to 0.4.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-cache-helper',
-    version='0.3.3',
+    version='0.4.0',
     description='Helps cache stuff',
     author='YCharts',
     author_email='operator@ycharts.com',
