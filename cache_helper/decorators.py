69c7d7726b4759427589ecc43ca285c75e10bfb0
Hyuckin Lim <hyuckin@Hyuckins-MacBook-Pro.local>
2021-08-05 14:49:09 -0400

    add docstring, rename helper invalidate to _invalidate

--- cache_helper/decorators.py
+++ cache_helper/decorators.py
@@ -17,7 +17,7 @@ def cached(timeout):
 
         @wraps(func)
         def wrapper(*args, **kwargs):
-            function_cache_key = utils. get_function_cache_key(func_name, args, kwargs)
+            function_cache_key = utils.get_function_cache_key(func_name, args, kwargs)
             cache_key = utils.get_hashed_cache_key(function_cache_key)
 
             try:
@@ -32,6 +32,7 @@ def cached(timeout):
                 # cache system, handle it.
                 try:
                     cache.set(cache_key, value, timeout)
+
                 except CacheSetError:
                     pass
 
@@ -63,7 +64,7 @@ def cached_class_method(timeout):
         @wraps(func)
         def wrapper(*args, **kwargs):
             # skip the first arg because it will be the class itself
-            function_cache_key = utils. get_function_cache_key(func_name, args[1:], kwargs)
+            function_cache_key = utils.get_function_cache_key(func_name, args[1:], kwargs)
             cache_key = utils.get_hashed_cache_key(function_cache_key)
 
             try:
@@ -102,13 +103,30 @@ def cached_class_method(timeout):
 
 
 def cached_instance_method(timeout):
+    """
+    Fact 1: We need to store the instance as part of the cache key
+    Fact 2: To find the correct cache key to invalidate, we need to know the instance
+    Fact 3: If we don't store the instance as part of the wrapper class, users of this decorator would have to
+            pass `self` as the first argument to every call of invalidate
+
+    Conclusion: We want the wrapper class to be able to automatically include the instance as the first argument to
+                both `__call__` and `invalidate`
+
+    To take care of the above requirements, we override __get__ to use functools.partial to automatically include
+    obj as the first argument. See the comments in __get__ for more details.
+    """
+
     class wrapper:
         def __init__(self, func):
             self.func = func
 
         def __get__(self, obj, objtype):
+            # When a user calls the instance method, this partial object is what actually gets called.
+            # It behaves exactly like `__call__` with `obj` automatically included as the first argument.
             fn = functools.partial(self.__call__, obj)
-            fn.invalidate = functools.partial(self.invalidate, obj)
+            # When a user calls invalidate, this partial object is what actually gets called.
+            # It behaves exactly like `_invalidate` with `obj` automatically included as the first argument.
+            fn.invalidate = functools.partial(self._invalidate, obj)
 
             return fn
 
@@ -129,7 +147,7 @@ def cached_instance_method(timeout):
                     pass
             return value
 
-        def invalidate(self, *args, **kwargs):
+        def _invalidate(self, *args, **kwargs):
             """
             A method to invalidate a result from the cache.
             :param args: The args passed into the original function. This includes `self` for instance methods, and
